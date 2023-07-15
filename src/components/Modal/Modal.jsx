96d9c6b21cf2c61519efc406486f7fb8cdd71947
irabasara <125157770+irabasara@users.noreply.github.com>
2023-07-15 16:50:40 +0300

    delete comments with class

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -33,33 +33,3 @@ export const Modal = ({ onModalClose, largeImage }) => {
   );
 };
 
-// export class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', handleKeyDown);
-//     // window.addEventListener('click', this.handleBackdropClick);
-//   }
-
-//   handleKeyDown = e => {
-//     if (e.code === `Escape`) {
-//       this.props.onModalClose();
-//     }
-//   };
-
-//   handleBackdropeClick = e => {
-//     if (e.target === e.currentTarget) {
-//       this.props.onModalClose();
-//     }
-//   };
-
-//   render() {
-//     const { largeImageURL, tags } = this.props.largeImage;
-
-//     return (
-//       <div className={css.overlay} onClick={this.handleBackdropeClick}>
-//         <div>
-//           <img className={css.modal} src={largeImageURL} alt={tags} />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
